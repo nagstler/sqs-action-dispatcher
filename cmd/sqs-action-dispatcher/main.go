249d16dbc33d83b1d299edf6630ffc79ddb927fb
Nagendra Dhanakeerthi <nagendra.dhanakeerthi@gmail.com>
2023-05-08 00:30:00 +0530

    feat: Implement DLQ support for error handling in message processing
    - Update config.go to include SQS_DLQ_URL configuration
    - Add SendMessageToDLQ function in poller.go to send messages to DLQ
    - Modify the worker function in main.go to handle errors and send messages to DLQ when necessary
    
    This commit adds support for sending messages to a Dead Letter Queue (DLQ) when an error occurs during message processing. It updates the configuration to include the DLQ URL, adds a new function to send messages to the DLQ, and modifies the worker function to handle errors accordingly.

--- cmd/sqs-action-dispatcher/main.go
+++ cmd/sqs-action-dispatcher/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
-	"time"
+	"sync"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
@@ -13,32 +13,67 @@ import (
 	"github.com/nagstler/sqs-action-dispatcher/internal/poller"
 )
 
-func main() {
-	cfg := config.Load()
+const numberOfWorkers = 5
 
-	sess := session.Must(session.NewSession(&aws.Config{
-		Region: aws.String(cfg.AWSRegion),
-	}))
-
-	sqsClient := sqs.New(sess)
-	sqsPoller := poller.NewPoller(sqsClient, cfg.SQSQueueURL)
-
-	actionDispatcher := dispatcher.NewDispatcher()
+// worker function processes messages in a concurrent fashion using goroutines.
+// It polls messages from the SQS queue, dispatches actions, and deletes messages after successful processing.
+func worker(id int, wg *sync.WaitGroup, sqsPoller *poller.Poller, actionDispatcher *dispatcher.Dispatcher, dlqURL string) {
+	defer wg.Done()
 
 	for {
 		messages, err := sqsPoller.Poll()
 		if err != nil {
-			log.Printf("Error polling messages: %v", err)
+			log.Printf("Error polling messages by worker %d: %v", id, err)
+			continue
 		}
 
 		for _, msg := range messages {
-			fmt.Printf("Received message: %s\n", msg.Body)
+			fmt.Printf("Worker %d received message: %s\n", id, msg.Body)
 
-			if err := actionDispatcher.Dispatch(msg.Body); err != nil {
-				log.Printf("Error dispatching message: %v", err)
+			err := actionDispatcher.Dispatch(msg.Body)
+			if err != nil {
+				log.Printf("Error dispatching message by worker %d: %v", id, err)
+
+				// Send the message to DLQ and delete the message from the source queue
+				if err := sqsPoller.SendMessageToDLQ(msg.Body, dlqURL); err != nil {
+					log.Printf("Error sending message to DLQ by worker %d: %v", id, err)
+				}
+			}
+
+			// Delete the message from the source queue after processing (success or failure)
+			if err := sqsPoller.DeleteMessage(msg.ReceiptHandle); err != nil {
+				log.Printf("Error deleting message by worker %d: %v", id, err)
 			}
 		}
-
-		time.Sleep(5 * time.Second)
 	}
+}
+
+func main() {
+	// Load the configuration
+	cfg := config.Load()
+
+	// Create a new AWS session
+	sess := session.Must(session.NewSession(&aws.Config{
+		Region: aws.String(cfg.AWSRegion),
+	}))
+
+	// Create a new SQS client and poller instance
+	sqsClient := sqs.New(sess)
+	sqsPoller := poller.NewPoller(sqsClient, cfg.SQSQueueURL)
+
+	// Initialize the action dispatcher
+	actionDispatcher := dispatcher.NewDispatcher()
+
+	// Create a wait group to synchronize the worker goroutines
+	var wg sync.WaitGroup
+
+	dlqURL := cfg.SQSDLQURL
+	// Start the worker goroutines
+	for i := 1; i <= numberOfWorkers; i++ {
+		wg.Add(1)
+		go worker(i, &wg, sqsPoller, actionDispatcher, dlqURL)
+	}
+
+	// Wait for all worker goroutines to complete
+	wg.Wait()
 }
